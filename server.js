5c0e848aa06d926b721eece74bb32d9d82e9f0c6
Abu Bakar Wase <33964884+abubakarwase@users.noreply.github.com>
2020-03-26 23:37:25 +0500

    initial commit

--- server.js
+++ server.js
@@ -7,19 +7,47 @@ const todos = [
 ];
 
 const server = http.createServer((req, res) => {
-  //   const { headers, url, method } = req;
-  //   console.log(headers, url, method);
-  //   res.setHeader("content-type", "text-html");
-  res.setHeader("content-type", "application/json");
-  res.setHeader("X-Powered-By", "node.js");
-  //   res.write("<h1>Hello</h1>");
-  //   res.write("<h1>Hello again</h1>");
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todos
+  const { method, url } = req;
+  let body = [];
+  req
+    .on("data", chunk => {
+      body.push(chunk);
     })
-  );
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null
+      };
+
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      }
+      if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+
+        if (!id || !test) {
+          status = 400;
+          response.success = false;
+        } else {
+          status = 201;
+          response.success = true;
+          response.data = { id, text };
+          todos.push({ id, text });
+        }
+      }
+
+      res.writeHead(status, {
+        "content-type": "application/json",
+        "X-Powered-By": "node.js"
+      });
+
+      res.end(JSON.stringify(response));
+    });
 });
 
 const PORT = 5000;
